af6dfa2dd89e5602db530f719a2032b6d4820090
niuzhenhao <niuzhenhao@58.com>
2020-07-28 19:07:11 +0800

    update

---
+++ src/main/java/leetcode/A26K个一组翻转链表.java
@@ -0,0 +1,64 @@
+package leetcode;
+
+/**
+ * @author niuzhenhao
+ * @date 2020/7/28 17:54
+ * @desc
+ */
+
+public class A26K个一组翻转链表 {
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+
+        if (head == null || k == 1) {
+            return head;
+        }
+
+
+        ListNode go = new ListNode(-2);
+        ListNode re = new ListNode(-1);
+        re.next = head;
+        go = re;
+
+        while (go != null && go.next != null) {
+            int count = 0;
+            ListNode[] list = new ListNode[k];
+            ListNode start = go;
+            while (count < k) {
+                if (go.next != null) {
+                    list[count++] = go.next;
+                    go = go.next;
+                } else {
+                    break;
+                }
+            }
+            ListNode temp = go.next;
+            if (count < k) {
+                break;
+            }
+            for (int i = k - 1; i >= 0; i--) {
+                if (i >= 1) {
+                  list[i].next = list[i - 1];
+                }
+            }
+            list[0].next = temp;
+            go = list[0];
+            start.next = list[k - 1];
+        }
+        return re.next;
+    }
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode f = new ListNode(5);
+//        a.next = b;
+//        b.next = c;
+//        c.next = d;
+//        d.next = f;
+        System.out.println(reverseKGroup(a, 2));
+    }
+
+
+}
--- src/main/java/leetcode/A交换链表.java
+++ src/main/java/leetcode/A交换链表.java
@@ -1,6 +1,5 @@
 package leetcode;
 
-
 public class A交换链表 {
     public static ListNode swapPairs(ListNode head) {
 
@@ -15,18 +14,19 @@ public class A交换链表 {
         ListNode before = null;
 
         boolean first = true;
-        while (go.next != null && go.next.next != null ) {
-
+        while (go.next != null && go.next.next != null) {
             ListNode temp = null;
             ListNode second = go.next.next;
-
             temp = second.next;
             second.next = go.next;
             second.next.next = null;
+            //如果后面还有
             if (temp != null && temp.next != null) {
                 go.next = temp;
+             //后面只有一个了
             } else {
                 second.next.next = temp;
+                go.next = null;
                 if (before != null) {
                     before.next = second;
                     break;
@@ -50,8 +50,8 @@ public class A交换链表 {
         ListNode c = new ListNode(3);
         ListNode d = new ListNode(4);
         ListNode f = new ListNode(5);
-        a.next = b;
-        b.next = c;
+//        a.next = b;
+//        b.next = c;
 //        c.next = d;
 //        d.next = f;
         System.out.println(swapPairs(a));
