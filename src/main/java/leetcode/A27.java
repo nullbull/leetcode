ea374433db684277b24d8f2daa31bc76b2e68cc3
niuzhenhao <niuzhenhao@58.com>
2020-07-28 19:28:07 +0800

    update

--- src/main/java/leetcode/A27.java
+++ src/main/java/leetcode/A27.java
@@ -31,6 +31,6 @@ public class A27 {
     }
 
     public static void main(String[] args) {
-        removeDuplicates(new int[]{1,1,2});
+        removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
     }
 }
