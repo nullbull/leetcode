1600b902f1b32b7049e62d3eace6d2c778b05cd2
牛贞昊 <niuzhenhao@58.com>
2020-09-27 22:58:59 +0800

    right answer

--- src/main/java/leetcode/A70爬楼梯.java
+++ src/main/java/leetcode/A70爬楼梯.java
@@ -22,12 +22,12 @@ public class A70爬楼梯 {
     }*/
 
     public int climbStairs(int n) {
-        int[] dp = new int[n];
+        int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n - 1];
+        return dp[n ];
     }
 
 
