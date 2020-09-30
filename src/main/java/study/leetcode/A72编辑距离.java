250dec3d3b7112247d74e2fd5bc2e80727df8114
niuzhenhao <niuzhenhao@58.com>
2020-09-30 10:27:15 +0800

    updte

--- src/main/java/study/leetcode/A72编辑距离.java
+++ src/main/java/study/leetcode/A72编辑距离.java
@@ -8,26 +8,44 @@ package study.leetcode;
 
 public class A72编辑距离 {
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
-        int dp[][] = new int[m][n];
-        int mm = 0;
-        int nn = 0;
-        int max = -1;
-        for (int i = 0; i < m; i ++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                }
-                if (word1.charAt(i) == word2.charAt(j)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
 
-                    if (dp[i][j] > max) {
 
-                    }
+        if ((word1 == null || word1.length() == 0)&& word2 != null) {
+            return word2.length();
+        }
+        if ((word2 == null || word2.length() == 0)&& word1 != null) {
+            return word1.length();
+        }
+
+        char[] c1 = word1.toCharArray();
+        char[] c2 = word2.toCharArray();
+
+        int m = c1.length;
+        int n = c2.length;
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i ++) {
+            for (int j = 1; j <= n; j++) {
+                if (c1[i-1] != c2[j-1]) {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j-1], dp[i-1][j-1]), dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j-1];
                 }
             }
         }
-        return 0;
+        return dp[m][n];
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new A72编辑距离().minDistance("horse", "ros"));
     }
 }
