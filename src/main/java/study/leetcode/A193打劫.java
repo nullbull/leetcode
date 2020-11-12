9f94d59de485efa1990fed69e586346464a86e61
niuzhenhao <niuzhenhao@58.com>
2020-11-12 19:31:43 +0800

    动态规划

---
+++ src/main/java/study/leetcode/A193打劫.java
@@ -0,0 +1,32 @@
+package com.bj58.mism.api.meishih5.mudu.controllers;
+
+/**
+ * @author niuzhenhao
+ * @date 2020/11/11 9:55
+ * @desc
+ */
+
+public class A193打劫 {
+
+
+  // 1 2 3 1
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length + 1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        if (nums.length > 2) {
+            for (int i = 2; i < nums.length; i++) {
+                dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            }
+        }
+
+        return dp[nums.length -1];
+
+    }
+}
