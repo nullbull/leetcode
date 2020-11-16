e978bd8b2f3b99b8cd044fdfd59d00f46b030acb
niuzhenhao <niuzhenhao@58.com>
2020-11-16 20:13:10 +0800

    两道简单题

---
+++ src/main/java/study/leetcode/A215数组中第k大元素.java
@@ -0,0 +1,29 @@
+package com.bj58.mism.api.video.job;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+/**
+ * @author niuzhenhao
+ * @date 2020/11/16 20:01
+ * @desc
+ */
+
+public class A215数组中第k大元素 {
+
+    public int findKthLargest(int[] nums, int k) {
+        ArrayList<Integer> list = new ArrayList<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+        list.sort(Integer::compareTo);
+        Collections.reverse(list);
+        return list.get(k -1 );
+    }
+
+    public static void main(String[] args) {
+        new A215数组中第k大元素().findKthLargest(new int[]{3,2,3,1,2,4,5,5,6, 4}, 4);
+    }
+}
