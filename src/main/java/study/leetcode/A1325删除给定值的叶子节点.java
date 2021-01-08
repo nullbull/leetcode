2f873d4dd0b4bb6ab39b685d080c9d5c770d378a
niuzhenhao <niuzhenhao@58.com>
2021-01-08 14:52:54 +0800

    update

--- src/main/java/study/leetcode/A1325删除给定值的叶子节点.java
+++ src/main/java/study/leetcode/A1325删除给定值的叶子节点.java
@@ -4,25 +4,77 @@ import java.util.Stack;
 
 public class A1325删除给定值的叶子节点 {
 
-
     public TreeNode removeLeafNodes(TreeNode root, int target) {
 
+        root = dfs(root, target);
+
+        return root;
+    }
+
+    public TreeNode dfs(TreeNode root, int target) {
+        if (root == null) {
+            return null;
+        }
+//        if (root.left == null && root.right == null && root.val == target) {
+//            return null;
+//        }
+
+        root.left = dfs(root.left, target);
+        root.right = dfs(root.right, target);
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+        return root;
+    }
+
+
+    public TreeNode removeLeafNodes2(TreeNode root, int target) {
+
         Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        TreeNode start = root;
-        while (!stack.isEmpty()) {
-            while (start != null) {
-                stack.push(start);
-                start = start.left;
+        TreeNode cur = root;
+        boolean isLeft =  true;
+        TreeNode father = root;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+                isLeft = true;
             }
-            start = stack.pop();
-            TreeNode father = stack.peek();
-            if (start.val == target && isLift(start)) {
-                father.left = null;
+            if(!stack.isEmpty()) {
+                cur = stack.pop();
+                if (cur.val == target && isLift(cur)) {
+                    if (!stack.isEmpty()) {
+                        if (isLeft) {
+                            stack.peek().left = null;
+                        } else {
+                            stack.peek().right = null;
+                        }
+                    } else {
+                        if (isLeft) {
+                            father.left = null;
+                        } else {
+                            father.right = null;
+                        }
+                    }
+                }
+                cur = cur.right;
+                isLeft = false;
             }
-            if (father.right != null) {
-                start = father.right;
-            }
+//            TreeNode father = stack.peek();
+//            if (cur.val == target && isLift(cur)) {
+//                if (isLeft) {
+//                    father.left = null;
+//                } else {
+//                    father.right = null;
+//                }
+//
+//            }
+//            if (father.right != null) {
+//                cur = father.right;
+//                isLeft = false;
+//            } else {
+//                cur = null;
+//            }
         }
         return root;
     }
@@ -32,5 +84,12 @@ public class A1325删除给定值的叶子节点 {
     }
 
 
+
+
+    public static void main(String[] args) {
+//        TreeNode root = TreeNodeBulider.treeNode(new Integer[]{1,2,3,2,null,2,4});
+        TreeNode root = TreeNodeBulider.treeNode(new Integer[]{1,3, 2, 2, null, null ,3});
+        new A1325删除给定值的叶子节点().removeLeafNodes(root, 3);
+    }
 
 }
---
+++ src/main/java/study/leetcode/TreeNodeBulider.java
@@ -0,0 +1,61 @@
+package study.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author niuzhenhao
+ * @date 2021/1/8 10:16
+ * @desc 根据数组构建 二叉平衡树
+ */
+
+public class TreeNodeBulider {
+
+    public  static TreeNode treeNode(Integer[] nums) {
+        int cur = 2;
+        int sum = 1;
+        List<TreeNode> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == null) {
+                list.add(null);
+            } else {
+                list.add(new TreeNode(nums[i]));
+            }
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(list.get(0));
+        while (!queue.isEmpty()) {
+            List<TreeNode> temp = new ArrayList<>();
+            for (int i = sum; i < cur + sum && i < nums.length && !queue.isEmpty(); i += 2) {
+                TreeNode father = queue.poll();
+                if (i < list.size()) {
+                    father.left = list.get(i);
+                    temp.add(list.get(i));
+                }
+                if (i + 1 < list.size()) {
+                    father.right = list.get(i+1);
+                    temp.add(list.get(i+1));
+                }
+            }
+            queue.addAll(temp);
+            sum += cur;
+            cur = cur * 2;
+            if (sum > nums.length) {
+                break;
+            }
+        }
+
+        return list.get(0);
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = treeNode(new Integer[]{1, 2, 3, 4, 5, 6});
+        System.out.println(node);
+    }
+
+
+}
